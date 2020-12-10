f53823227ecbacded15a7a86eaca36ae3a88df9f
verityyt <joshua-schmidt@online.de>
2020-12-10 16:01:10 +0100

    :construction: Add local database for all messages

--- src/server/Database.kt
+++ src/server/Database.kt
@@ -2,4 +2,22 @@ package server
 
 class Database {
 
+    var messages = java.util.HashMap<String, HashMap<Long, String>>()
+
+    fun add(user: String, message: String) {
+
+        if(messages.containsKey(user)) {
+            val hashmap = messages[user]!!
+            hashmap[System.currentTimeMillis()] = message
+
+            messages[user] = hashmap
+        }else {
+            val hashmap = HashMap<Long, String>()
+            hashmap[System.currentTimeMillis()] = message
+
+            messages[user] = hashmap
+        }
+
+    }
+
 }
