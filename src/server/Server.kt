64f3caaaf69cafedf216abbcc241dc68e43a70c7
verityyt <joshua-schmidt@online.de>
2020-12-10 15:59:47 +0100

    :construction: Add ktor server

--- src/server/Server.kt
+++ src/server/Server.kt
@@ -6,8 +6,22 @@ import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import server.Database
-import java.text.SimpleDateFormat
 
 fun main() {
+
+    val database = Database()
+    database.import()
+
+    val server = embeddedServer(Netty, port = 8080) {
+
+        routing {
+            get("/") {
+                call.respondText("Ready!")
+            }
+        }
+
+    }
+
+    server.start(wait = true)
 
 }
