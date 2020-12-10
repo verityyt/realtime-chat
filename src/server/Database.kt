982e79a283a78e2c40e00678a21dfd9040a4f5f5
verityyt <joshua-schmidt@online.de>
2020-12-10 16:01:42 +0100

    :construction: Add import and export function

--- src/server/Database.kt
+++ src/server/Database.kt
@@ -1,8 +1,26 @@
 package server
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+
 class Database {
 
     var messages = java.util.HashMap<String, HashMap<Long, String>>()
+
+    val file = File("message.history")
+
+    fun import() {
+        if(!file.exists()) {
+            file.writeText("[]")
+        }
+
+        val typeToken = object : TypeToken<HashMap<String, HashMap<Long, String>>>() {}.type
+        messages = Gson().fromJson(file.readText(), typeToken)
+
+    }
+
+    fun export() = file.writeText(Gson().toJson(messages))
 
     fun add(user: String, message: String) {
 
